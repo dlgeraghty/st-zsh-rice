ea13ad40b879a50b47b060375769d82813ce1083
dlgeraghty <dvd.loge@gmail.com>
2020-04-07 18:00:16 +0200

    fixing

--- st-zsh-rice.sh
+++ st-zsh-rice.sh
@@ -1,22 +1,23 @@
 #repo:
 cd
-git clone https://github.com/dlgeraghty/st-zsh-rice.git &&
+#git clone https://github.com/dlgeraghty/st-zsh-rice.git &&
 
 #fonts:
 cd st-zsh-rice &&
-cp IosevkaNerdFont.ttf ~/usr/share/fonts/misc/ &&
-fc-cache &&
+sudo mkdir /usr/local/share/fonts/misc
+sudo cp IosevkaNerdFontComplete.ttf /usr/local/share/fonts/misc/ &&
+fc-cache -f -v &&
 [ $(fc-list | grep "Iosevka Nerd" | wc -l) -lt 1 ] && echo "Nerd Font installed Correctly" &&
 sudo apt-get install fonts-inconsolata -y || sudo pacman -S ttf-inconsolata
 
 #zsh:
-sudo pacman -S zsh -y || sudo apt install zsh -y &&
-sudo chsh -s $(whereis zsh) &&
+sudo pacman -S zsh -y || sudo apt install zsh -y ;
+	chsh -s /bin/zsh $(whoami) &&
 cd &&
-mv ~/.zshrc ~/.zshrc.old
-mv st-zsh-rice/.zshrc ~/ &&
-mkdir /usr/share/zsh &&
-mkdir /usr/share/zsh/plugins &&
+[ $(ls ~/.zshrc) | wc-l ) -lt 0 ] && mv ~/.zshrc ~/.zshrc.old
+cp st-zsh-rice/.zshrc ~/ &&
+mkdir /usr/share/zsh ;
+mkdir /usr/share/zsh/plugins ;
 cd /usr/share/zsh/plugins &&
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git &&
 cd &&
